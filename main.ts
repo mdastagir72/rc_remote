7da414f888a10ee420cc660e18c5a102fba41788
Abdul Rahman Dastagir <102039148+mdastagir72@users.noreply.github.com>
2022-08-04 21:45:14 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+radio.setGroup(100)
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    if (input.isGesture(Gesture.LogoUp)) {
+        radio.sendNumber(1)
+    }
+    if (input.isGesture(Gesture.LogoDown)) {
+        radio.sendNumber(2)
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        radio.sendNumber(3)
+    }
+    if (input.isGesture(Gesture.TiltLeft)) {
+        radio.sendNumber(4)
+    }
+})
